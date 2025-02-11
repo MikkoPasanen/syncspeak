c039353a78334d34e3e9c230009a4440a5fc8f9e
Mikko Pasanen <113339319+MikkoPasanen@users.noreply.github.com>
2025-02-11 02:01:40 +0200

    feat(backend): add new post endpoint to handle marking messages as read

--- backend/src/main/java/syncspeak/backend/controller/ChatController.java
+++ backend/src/main/java/syncspeak/backend/controller/ChatController.java
@@ -1,15 +1,16 @@
 package syncspeak.backend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.*;
 import syncspeak.backend.entity.Message;
 import syncspeak.backend.service.ChatService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,10 +19,37 @@ import java.util.UUID;
 @RequestMapping("/api/chat")
 public class ChatController {
     private final ChatService chatService;
+    private final SimpMessagingTemplate messagingTemplate;
+
 
     // Get chat history between 2 users
     @GetMapping("/{senderId}/{receiverId}")
-    public List<Message> getChatHistory(@PathVariable UUID senderId, @PathVariable UUID receiverId) {
-        return chatService.getChatHistory(senderId, receiverId);
+    public ResponseEntity<List<Message>> getChatHistory(@PathVariable UUID senderId, @PathVariable UUID receiverId) {
+        List<Message> messages = chatService.getChatHistory(senderId, receiverId);
+        return new ResponseEntity<>(messages, HttpStatus.OK);
+    }
+
+    // Mark old messages as read
+    @PostMapping("/mark-read")
+    public ResponseEntity<?> markMessagesAsRead(@RequestBody List<UUID> messageIds) {
+        List<Message> updatedMessages = new ArrayList<>();
+
+        for (UUID messageId : messageIds) {
+            chatService.findById(messageId).ifPresent(message -> {
+                if (!message.isHasBeenRead()) {
+                    message.setHasBeenRead(true);
+                    chatService.saveMessage(message);
+                    updatedMessages.add(message);
+                }
+            });
+        }
+
+        // Notify sender that the messages have been read
+        for (Message msg : updatedMessages) {
+            String destination = "/user/" + msg.getSenderId().toString() + "/" + msg.getReceiverId().toString() + "/queue/messages";
+            messagingTemplate.convertAndSend(destination, msg);
+        }
+
+        return ResponseEntity.ok().build();
     }
 }
